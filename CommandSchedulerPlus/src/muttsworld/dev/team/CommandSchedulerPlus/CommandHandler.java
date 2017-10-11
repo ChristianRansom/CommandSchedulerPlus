b0acd71e9db92643036dfb633c5cf2318c805933
Christian Ransom <christian.ransom123@gmail.com>
2017-10-11 18:03:54 +0800

    Set up basic command handling

--- CommandSchedulerPlus/src/muttsworld/dev/team/CommandSchedulerPlus/CommandHandler.java
+++ CommandSchedulerPlus/src/muttsworld/dev/team/CommandSchedulerPlus/CommandHandler.java
@@ -1,9 +1,11 @@
 package muttsworld.dev.team.CommandSchedulerPlus;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 
 public class CommandHandler implements CommandExecutor{
@@ -12,7 +14,20 @@ public class CommandHandler implements CommandExecutor{
         if (sender instanceof Player) {
             Player player = (Player) sender;
             // Here we need to give items to our player
+            
+            // Create a new ItemStack (type: diamond)
+            ItemStack diamond = new ItemStack(Material.DIAMOND);
+
+            // Create a new ItemStack (type: brick)
+            ItemStack bricks = new ItemStack(Material.BRICK);
+
+            // Set the amount of the ItemStack
+            bricks.setAmount(20);
+
+            // Give the player our items (comma-separated list of all ItemStack)
+            player.getInventory().addItem(bricks, diamond);
         }
+        
 
         // If the player (or console) uses our command correct, we can return true
         return true;
