fd5fa1f37448c1ddc9087f4d4523f3bd118e57bd
Christian Ransom <christian.ransom123@gmail.com>
2017-10-11 19:03:53 +0800

    Set up basic config file handling

--- CommandSchedulerPlus/src/muttsworld/dev/team/CommandSchedulerPlus/CommandSchedulerPlus.java
+++ CommandSchedulerPlus/src/muttsworld/dev/team/CommandSchedulerPlus/CommandSchedulerPlus.java
@@ -1,17 +1,34 @@
 package muttsworld.dev.team.CommandSchedulerPlus;
 
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 
 public class CommandSchedulerPlus extends JavaPlugin {
 	
+	 FileConfiguration config = getConfig();
+	
     @Override
     public void onEnable() {
     	
     	//sets up the CommanadHandler class to handle csp commands
+        
+        config.addDefault("ScheduleTimer", 100);
+        config.options().copyDefaults(true);
+        saveConfig();
+        
+        
+        if (config.getDouble("ScheduleTimer") != 100) {
+        	System.out.println("ScheduledTimer doesn't equal 100!");
+        }
+        else {
+        	System.out.println("ScheduledTimer equals 100!");
+        }
+        
         this.getCommand("csp").setExecutor(new CommandHandler());
+
     }
 
     @Override
