138d5d1d845c03e153d2b73c7b8fcfff484a66e5
Christian Ransom <christian.ransom123@gmail.com>
2017-10-12 09:44:57 +0800

    Set up basic command sending

--- CommandSchedulerPlus/src/muttsworld/dev/team/CommandSchedulerPlus/CommandHandler.java
+++ CommandSchedulerPlus/src/muttsworld/dev/team/CommandSchedulerPlus/CommandHandler.java
@@ -1,9 +1,13 @@
 package muttsworld.dev.team.CommandSchedulerPlus;
 
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -13,24 +17,19 @@ public class CommandHandler implements CommandExecutor{
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            // Here we need to give items to our player
-            
-            // Create a new ItemStack (type: diamond)
-            ItemStack diamond = new ItemStack(Material.DIAMOND);
 
-            // Create a new ItemStack (type: brick)
-            ItemStack bricks = new ItemStack(Material.BRICK);
-
-            // Set the amount of the ItemStack
-            bricks.setAmount(20);
-
-            // Give the player our items (comma-separated list of all ItemStack)
-            player.getInventory().addItem(bricks, diamond);
-            MainThread mainthread = new MainThread();
-            mainthread.start();
         }
+/*        StringBuilder builder = new StringBuilder();
+        for(String s : args) {
+            builder.append(s);
+        }
+        String str = builder.toString();*/
+        String str = String.join(" ", args);
         
-
+        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();;
+        Bukkit.dispatchCommand(console, str);
+        System.out.print(str);
+        
         // If the player (or console) uses our command correct, we can return true
         return true;
     }
